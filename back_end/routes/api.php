afc5f45560eb2c859a65b1d6c7c79df1e7474025
boshra <boshra@gmail.com>
2025-03-24 20:51:24 +0300

    upload file back end with auth

---
+++ back_end/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])
+        ->middleware('auth:api');
+    Route::post('refresh', [AuthController::class, 'refresh'])
+        ->middleware('auth:api');
+});
